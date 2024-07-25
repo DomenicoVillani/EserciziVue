6d403b572862df530300d532e9177ffefe5557f6
Domenico <domenico.villani@gmail.com>
2024-07-25 17:27:15 +0200

    continuo esercizi prime vue esercizio notifiche con sessionstorage

--- src/views/NotificheView.vue
+++ src/views/NotificheView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import Button from 'primevue/button';
 import { useToast } from 'primevue/usetoast';
 
@@ -9,18 +9,30 @@ const valuenotifiche = ref(0);
 
 const show = () => {
     toast.add({ severity: 'info', summary: 'Notifica', detail: 'Hai ricevuto una notifica', life: 3000 });
-    console.log(toast);
     valuenotifiche.value++;
-}
+    sessionStorage.setItem('notificationsCount', valuenotifiche.value.toString());
+};
+
+const loadNotificationsCount = () => {
+    const savedCount = sessionStorage.getItem('notificationsCount');
+    if (savedCount !== null) {
+        valuenotifiche.value = parseInt(savedCount, 10);
+    } else {
+        valuenotifiche.value = 0;
+    }
+};
+
+onMounted(() => {
+    loadNotificationsCount();
+});
 </script>
 
 <template>
     <div>
-        <Button label="Inviami una notifica" @click="show()"/>
+        <Button label="Inviami una notifica" @click="show"/>
         <p>Notifiche ricevuto: {{ valuenotifiche }}</p>
     </div>
 </template>
 
 <style scoped>
-
 </style>
